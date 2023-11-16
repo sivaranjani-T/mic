c84297c9a30d4cc829613eab254c0b5ed5f17598
sivaranjani-T <thangaraj.sivaranjani@divum.in>
2023-11-16 12:25:34 +0530

    second commit in service

--- src/main/java/com/example/service/ServiceApplication.java
+++ src/main/java/com/example/service/ServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceApplication {
-
+  // microservices using eureka and fleignClient
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
     }
